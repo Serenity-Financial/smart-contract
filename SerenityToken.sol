82b27d8b9ac1ebb012ebfd62040086a836a52c7d
Vasily Alexeev <va@uptrader.us>
2017-11-16 15:23:09 +0000

    Removed locks

--- SerenityToken.sol
+++ SerenityToken.sol
@@ -24,21 +24,11 @@ contract SerenityToken is ISerenityToken, ERC20Token, Owned {
   uint256 public initialSupply = 3500000 ether;
   uint256 public totalSoldTokens = 0;
   uint256 public totalProjectToken;
-  uint256 private totalLockToken;
-  bool public transfersEnabled = false; 
+  bool public transfersEnabled = false;
 
   mapping (address => bool) private fundingWallets;
-  mapping (address => allocationLock) public allocations;
-
-  struct allocationLock {
-    uint256 value;
-    uint256 end;
-    bool locked;
-  }
 
   event Finalize(address indexed _from, uint256 _value);
-  event Lock(address indexed _from, address indexed _to, uint256 _value, uint256 _end);
-  event Unlock(address indexed _from, address indexed _to, uint256 _value);
   event DisableTransfers(address indexed _from);
 
   function SerenityToken() ERC20Token("SERENITY INVEST", "SERENITY", 18) public {
@@ -80,39 +70,6 @@ contract SerenityToken is ISerenityToken, ERC20Token, Owned {
     return super.transferFrom(_from, _to, _value);
   }
 
-  function lock(address _to, uint256 _value, uint256 _end) internal validAddress(_to) onlyOwner returns (bool) {
-    require(_value > 0);
-
-    assert(totalProjectToken > 0);
-    totalLockToken = totalLockToken.add(_value);
-    assert(totalProjectToken >= totalLockToken);
-
-    require(allocations[_to].value == 0);
-
-    // Assign a new lock.
-    allocations[_to] = allocationLock({
-      value: _value,
-      end: _end,
-      locked: true
-    });
-
-    Lock(this, _to, _value, _end);
-
-    return true;
-  }
-
-  function unlock() external {
-    require(allocations[msg.sender].locked);
-    require(now >= allocations[msg.sender].end);
-    
-    balanceOf[msg.sender] = balanceOf[msg.sender].add(allocations[msg.sender].value);
-
-    allocations[msg.sender].locked = false;
-
-    Transfer(this, msg.sender, allocations[msg.sender].value);
-    Unlock(this, msg.sender, allocations[msg.sender].value);
-  }
-
   function getTotalSoldTokens() public constant returns (uint256) {
     uint256 result = 0;
     result = result.add(maxSaleToken.sub(balanceOf[fundingWallet]));
@@ -128,12 +85,12 @@ contract SerenityToken is ISerenityToken, ERC20Token, Owned {
 
     totalProjectToken = totalSoldTokens.mul(15).div(100);
 
-    lock(0x47c8F28e6056374aBA3DF0854306c2556B104601, totalProjectToken, now);
-    
     // Zeroing a cold wallet.
     balanceOf[fundingWallet] = 0;
-    balanceOf[0x47c8F28e6056374aBA3DF0854306c2556B104601] = 0;
     balanceOf[0xCAD0AfB8Ec657D0DB9518B930855534f6433360f] = 0;
+
+    // Shareholders/bounties
+    balanceOf[0x47c8F28e6056374aBA3DF0854306c2556B104601] = totalProjectToken;
 
     // End of crowdfunding.
     fundingEnabled = false;

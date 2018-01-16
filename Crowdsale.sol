874301fe9222e7e73d1300686b3ad604e27ad768
Vasily Alexeev <va@uptrader.us>
2018-01-16 18:27:25 +0300

    Fix solidity version to 0.4.18

--- Crowdsale.sol
+++ Crowdsale.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity 0.4.18;
 
 import './SerenityToken.sol';
 import './SafeMath.sol';
--- Owned.sol
+++ Owned.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity 0.4.18;
 
 contract IOwned {
   function owner() public view returns (address) { owner; }
